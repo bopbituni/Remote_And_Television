30022360edb7e3345bc48baa58273d00adc8ffef
bopbituni <thanhngau199@gmail.com>
2019-08-19 16:50:01 +0700

    this is remote file of Television

---
+++ remote.js
@@ -0,0 +1,10 @@
+let Remote = function (amLuong) {
+
+    this.amLuong = amLuong;
+    this.getAmLuong = function () {
+        return this.amLuong;
+    };
+    this.setAmLuong = function (amLuong) {
+        this.amLuong = amLuong;
+    }
+};
